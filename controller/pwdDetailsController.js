8e2e2e92abfa9d1c8f3b11988fa99ebe0cec5478
sathyasasi <sathyasasi@users.noreply.github.com>
2016-03-23 12:07:21 +0530

    Update pwdDetailsController.js

--- controller/pwdDetailsController.js
+++ controller/pwdDetailsController.js
@@ -13,12 +13,12 @@ var Student = require('../models/student.js');
 exports.forgotPassword = function(req, res, next){
 var email = req.params.email;
 console.log(email);
-Staff.findOne({'email':email}, function(err, user){
+Staff.findOne({'email':email}, function(err, staff){
   if(err){
     res.status(400).send('Error looking up for email');
     return next();
-  } else if(user) {
-    mail.sendMail(user.email, 0, user.name, user._id, function(result){
+  } else if(staff) {
+    mail.sendMail(staff.email, 0, staff.name, staff._id, function(result){
         if(result == 1){
           res.status(400).send('Error sending mail');
           return next();
@@ -34,12 +34,12 @@ Staff.findOne({'email':email}, function(err, user){
 })
 }
 
-//sending the password file
+//sending the password file for staff
 exports.sendPasswordFile = function(req, res, next) {
   var id = req.params.id;
   var link = "http://127.0.0.1:27017/api/forgotpassword/"+id;
-  Staff.findById(id,function(err,user){
-    if(user != null && user != ""){
+  Staff.findById(id,function(err,staff){
+    if(staff != null && staff != ""){
     console.log(link);
     res.sendFile(path.resolve(__dirname, '../views', 'index.html'));
     return next();
@@ -51,20 +51,81 @@ exports.sendPasswordFile = function(req, res, next) {
 });
 }
 
-//Resett Password
+//Resett Password for staff
 exports.changePassword = function(req,res,next){
  var id = req.body._id;
  var _password = req.body._password;
- Staff.findById(id,function(err, user){
+ Staff.findById(id,function(err, staff){
  if(err) return next(err);
- user._password = _password;
- user.save(function(err, user)
+ staff._password = _password;
+ staff.save(function(err, staff)
    {
     if(err) throw err;
-     console.log(user.name);
-     res.status(200).send("Password Successfully resetted"+ user._password);
-     console.log(user._password);
+     console.log(staff.name);
+     res.status(200).send("Password Successfully resetted"+ staff._password);
+     console.log(staff._password);
      return next();
    });
  });
 }
+//Forgot password for student
+exports.forgotPassword = function(req, res, next){
+var email = req.params.email;
+console.log(email);
+Student.findOne({'email':email}, function(err, student){
+  if(err){
+    res.status(400).send('Error looking up for email');
+    return next();
+  } else if(user) {
+    mail.sendMail(student.email, 0, student.name, student._id, function(result){
+        if(result == 1){
+          res.status(400).send('Error sending mail');
+          return next();
+        } else {
+        res.status(200).send('Mail Sent');
+        return next();
+      }
+      });
+  } else {
+    res.status(400).send('No user found');
+    return next();
+  }
+})
+
+}
+
+//sending the password file for student
+exports.sendPasswordFile = function(req, res, next) {
+  var id = req.params.id;
+  var link = "http://127.0.0.1:52320/api/forgotpassword/"+id;
+  Student.findById(id,function(err,student){
+    if(student != null && student != ""){
+    console.log(link);
+    res.sendFile(path.resolve(__dirname, '../views', 'index.html'));
+    return next();
+  }
+  else {
+    res.status(200).send("Sorry ,You are not an authorised user");
+    return next();
+  }
+});
+}
+
+//Resetting Password for student
+exports.changePassword = function(req,res,next){
+ var id = req.body._id;
+ var _password = req.body._password;
+ Student.findById(id,function(err, student){
+ if(err) return next(err);
+ student._password = _password;
+ student.save(function(err, student)
+   {
+    if(err) throw err;
+     console.log(student.name);
+     res.status(200).send("Password Successfully resetted"+ student._password);
+     console.log(student._password);
+     return next();
+   });
+ });
+}
+
